5510dce620b29567e31da0fe319cd6b6ab9289a4
Rayzen7 <nugrahasetio6@gmail.com>
2025-03-05 16:14:27 +0700

    update

--- JS/download.js
+++ JS/download.js
@@ -1,11 +1,14 @@
 window.addEventListener('scroll', function() {
     const scroll = this.window.scrollY;
     const barSuccess = this.document.querySelector('.quality-progress-bar-success');
+    const barDesignSuccess = this.document.querySelector('.design-progress');
     console.log(scroll)
 
     if (scroll >= 1400) {
         barSuccess.classList.add('active');
+        barDesignSuccess.classList.add('active');
     } else {
         barSuccess.classList.remove('active'); 
+        barDesignSuccess.classList.remove('active');
     }
 });
